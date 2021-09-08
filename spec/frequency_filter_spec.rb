984abb37b2dfaa0a6d3862b5154c8e1815452e48
francescoFH <francescofacca@gmail.com>
2021-09-08 01:31:21 +0100

    Added failing test to check if sound  wave is returned as it is if it's not below lowest frequency

--- spec/frequency_filter_spec.rb
+++ spec/frequency_filter_spec.rb
@@ -4,7 +4,12 @@ describe Frequency do
 
   subject(:frequency) { described_class.new }
 
-  it "returns a sound wave as it is" do
+  it "returns a sound wave" do
     expect(frequency.filter([10, 20])).to eq [10, 20]
   end
+
+  it "returns a sound wave unchanged when it's not below lowest frequency" do
+    expect(frequency.filter([10, 30], 20)).to eq [10, 30]
+  end
+
 end
